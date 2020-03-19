3b61500e27c2b029dd5459521c1506ca7a7a5922
Dillon McGovern <dillonm800@gmail.com>
2020-03-19 15:34:09 -0400

    getting href to work on server

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,10 +3,12 @@ import {Button} from 'react-bootstrap'
 
 const Login = ( props ) => {
 
+// let deployed_server = 'https://https://intense-dawn-23012.herokuapp.com'
+
     return(
         <div>
 
-            <a href='http://localhost:8888/login'>
+            <a href='https://https://intense-dawn-23012.herokuapp.com/login'>
             <Button variant='outline-primary'>Log In to Spotify</Button>
             </a>
         </div>
